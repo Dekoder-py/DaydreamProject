9e641c96b27bddcc109d336c0811ea596dd8b7db
Dekoder-py <dekod3r.py@gmail.com>
2025-09-28 09:55:09 +1300

    add jump sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var jump_sound: AudioStreamPlayer = $JumpSound
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -13,6 +14,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
