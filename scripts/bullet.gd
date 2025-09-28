828c317e0201c2c5c0987bf7bcef45dceadd8eae
Dekoder-py <dekod3r.py@gmail.com>
2025-09-28 14:12:33 +1300

    add bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+@onready var timer: Timer = $Timer
+var direction = 1
+
+const SPEED = 800
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !direction:
+		direction = 1
+	timer.start()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += Vector2(1, 0) * delta * SPEED * direction
+	
+
+
+func _on_timer_timeout() -> void:
+	pass
+	
